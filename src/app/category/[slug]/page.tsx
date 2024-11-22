77c540877428c4a3e05868f81e0b85358d6f463e
MD Pabel <mdpabel385@gmail.com>
2024-11-22 18:10:44 +0600

    feat: static

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,4 +1,19 @@
 import Blogs from '@/components/blog/blogs';
+import { fetchCategories } from '@/lib/wordpress/fetch-category';
+import { WP_REST_API_Category } from 'wp-types';
+
+export async function generateStaticParams() {
+  const categories = await fetchCategories();
+
+  // Ensure posts is an array before passing it to BlogList
+  if (!Array.isArray(categories)) {
+    return [];
+  }
+
+  return categories.map((category: WP_REST_API_Category) => ({
+    slug: category.slug,
+  }));
+}
 
 type Props = {
   params: Promise<{ slug: string }>;
--- src/app/tag/[slug]/page.tsx
+++ src/app/tag/[slug]/page.tsx
@@ -1,4 +1,19 @@
 import Blogs from '@/components/blog/blogs';
+import { fetchTags } from '@/lib/wordpress/fetch-tags';
+import { WP_REST_API_Tag } from 'wp-types';
+
+export async function generateStaticParams() {
+  const tags = await fetchTags();
+
+  // Ensure posts is an array before passing it to BlogList
+  if (!Array.isArray(tags)) {
+    return [];
+  }
+
+  return tags.map((tag: WP_REST_API_Tag) => ({
+    slug: tag.slug,
+  }));
+}
 
 type Props = {
   params: Promise<{ slug: string }>;
