12f1b6ffec98d79c8cd9c670220cf9a8792a687b
MD Pabel <mdpabel385@gmail.com>
2024-11-22 22:42:35 +0600

    fix

--- src/app/preload-resources.tsx
+++ src/app/preload-resources.tsx
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom';
 
 export default function PreloadResources() {
   const basePath = process.env.BASE_PATH!;
-  ReactDOM.preload(`${basePath}/blog`, { as: 'image', fetchPriority: 'high' });
+  ReactDOM.preload(`${basePath}/blog`, {
+    as: 'document',
+    fetchPriority: 'high',
+  });
 
   return null;
 }
